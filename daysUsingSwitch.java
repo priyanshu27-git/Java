8db69a809a8699bcc894bb4e8898ec156332f2d8
priyanshu27-git <sahupriyanshu88a@gmail.com>
2025-04-11 11:10:41 +0530

    upadte-3

---
+++ daysUsingSwitch.java
@@ -0,0 +1,39 @@
+
+import java.util.*;
+
+public class daysUsingSwitch {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int day = scanner.nextInt();
+        String dayName = "";
+
+        switch (day) {
+            case 1:
+                dayName = "Sunday";
+                break;
+            case 2:
+                dayName = "Monday";
+                break;
+            case 3:
+                dayName = "Tuesday";
+                break;
+            case 4:
+                dayName = "Wednesday";
+                break;
+            case 5:
+                dayName = "Thursday";
+                break;
+            case 6:
+                dayName = "Friday";
+                break;
+            case 7:
+                dayName = "Saturday";
+                break;
+            default:
+            System.out.println("NA");
+        }
+        System.out.println(dayName);
+
+    }
+}
--- palindrome.java
+++ palindrome.java
@@ -1,22 +1,23 @@
 class palindrome {
     public static void main(String[] args)
     {
-        String str = "madam";
+        String str = "priyanshu";
         int i=0,j=str.length()-1;
         while(i<j)
         {
             if(str.charAt(i) != str.charAt(j))
             {
-                System.out.println("Not Palindrome");
                 break;
             }
             else
             {
                 i++;
                 j--;
-                System.out.println("Palindrome");
-                break;
             }
         }
+        if(i==j)
+        System.out.println("Palindrome");
+        else
+        System.out.println("Not Palindrome");
     }
 }
