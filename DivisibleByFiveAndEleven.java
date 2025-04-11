7c3e7a1e901bae9e67303600ffdc05ca2dbe017c
priyanshu27-git <sahupriyanshu88a@gmail.com>
2025-04-11 18:35:22 +0530

    java

---
+++ DivisibleByFiveAndEleven.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class DivisibleByFiveAndEleven {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        if (n % 5 == 0) 
+        {
+            System.out.println("Number is divisible by 5");
+        } else if (n % 11 == 0)
+         {
+            System.out.println("Number is divisible by 11");
+        } 
+        else 
+        {
+            System.out.println("Number is not divisible by 5 & 11");
+        }
+    }
+}
