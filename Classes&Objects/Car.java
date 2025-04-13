cbae028e8ba36a42b7fdb2c2aaed3d3d512021e8
priyanshu27-git <sahupriyanshu888a@gmail.com>
2025-04-13 22:49:40 +0530

    Classes and objects 1st commit

---
+++ Classes&Objects/Car.java
@@ -0,0 +1,8 @@
+public class Car {
+    String color;
+    public static void main(String[] args) {
+        Car car = new Car();
+        car.color = "Red";
+        System.out.println(car.color);
+    }
+}
---
+++ Classes&Objects/Student1.java
@@ -0,0 +1,6 @@
+public class Student1 {
+    public static void main(String[] args) {
+        Student1 student = new Student1();
+        System.out.println(student);
+    }
+}
