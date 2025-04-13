d125f891fd5064080e01aa97371072fd5bbf4589
priyanshu27-git <sahupriyanshu888a@gmail.com>
2025-04-13 23:15:41 +0530

    area of rectangle

---
+++ Classes&Objects/Rectangle.java
@@ -0,0 +1,9 @@
+public class Rectangle {
+    int length,width;
+    public static void main(String[] args) {
+        Rectangle rectangle = new Rectangle();
+        rectangle.length = 5;
+        rectangle.width = 8;
+        System.out.println(rectangle.length*rectangle.width);
+    }
+}
