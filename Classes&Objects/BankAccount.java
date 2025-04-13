dcd940fb664ee66a2171c7e863eb67a49037bdb8
priyanshu27-git <sahupriyanshu888a@gmail.com>
2025-04-13 23:29:44 +0530

    Simple Bank program

---
+++ Classes&Objects/BankAccount.java
@@ -0,0 +1,8 @@
+public class BankAccount {
+    int Balance;
+    public static void main(String[] args) {
+        BankAccount obj = new BankAccount();
+        obj.Balance = 1000;
+        System.out.println(obj.Balance);
+    }
+}
