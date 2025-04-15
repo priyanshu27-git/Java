16be824f3fde9fcc529307be5198b896d499bbb2
priyanshu27-git <sahupriyanshu888a@gmail.com>
2025-04-15 19:18:20 +0530

    Pattern day 1

---
+++ Patterns/pyramid.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+class pyramid {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        for(int i=0;i<n;i++){
+            for (int j = 0; j < n-i-1 ;j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j < 2*(i+1) ;j++) {
+                System.out.print("*");
+            }
+            for (int j = 0; j < n-i-1 ;j++) {
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
