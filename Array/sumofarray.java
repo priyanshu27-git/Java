e23efc069ec9deb37910dc11d09f941f3a876eed
priyanshu27-git <sahupriyanshu888a@gmail.com>
2025-04-16 14:03:40 +0530

    Day 4 training Done

--- Array/sumofarray.java
+++ Array/sumofarray.java
@@ -1,17 +1,20 @@
 
 import java.util.Scanner;
 
-public class inputArray {
+public class sumofarray {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int arr[] = new int[n];
+        int sum=0;
         System.out.println("Enter value in array"); 
         for (int i = 0; i < arr.length; i++) {
             arr[i] = sc.nextInt();
         }
         for (int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i]);
+            sum += arr[i]; 
         }
+
+        System.out.println(sum);
     }
 }
