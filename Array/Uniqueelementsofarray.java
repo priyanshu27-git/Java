0fce9debf2003bf94f14317719565eca750a4dba
priyanshu27-git <sahupriyanshu888a@gmail.com>
2025-04-19 15:48:38 +0530

    Detailed Unique elements in array

---
+++ Array/Uniqueelementsofarray.java
@@ -0,0 +1,34 @@
+public class Uniqueelementsofarray {
+    public static void main(String[] args) {
+        int [] arr1 = {1,2,3,4,22,12,34};
+        int [] arr2 = {3,4,5,4,11,43,54};
+        System.out.println("Unique elements in array 1");
+        for (int i = 0; i < arr1.length; i++) {
+            for (int j = 0; j < arr2.length; j++) {
+                if(arr1[i] != arr2[j])
+                {
+                if(j == arr2.length - 1)
+                {
+                    System.out.println(arr1[i] + " is Unique");
+                }
+            }
+                else
+                break;
+            }
+        }
+        System.out.println("Unique elements in array 2");
+        for (int i = 0; i < arr2.length; i++) {
+            for (int j = 0; j < arr1.length; j++) {
+                if(arr2[i] != arr1[j])
+                {
+                if(j == arr1.length - 1)
+                {
+                    System.out.println(arr2[i] + " is Unique");
+                }
+            }
+                else
+                break;
+            }
+        }
+    }
+}
