320e04933a8bf199ddf378f0fbc3f524f3477e9b
priyanshu27-git <sahupriyanshu888a@gmail.com>
2025-04-20 00:04:18 +0530

    2d array 1st commit

--- Array/Uniqueelementsofarray.java
+++ Array/Uniqueelementsofarray.java
@@ -1,33 +1,31 @@
+
 public class Uniqueelementsofarray {
+
     public static void main(String[] args) {
-        int [] arr1 = {1,2,3,4};
-        int [] arr2 = {3,4,5,4};
+        int[] arr1 = {1, 2, 3, 4};
+        int[] arr2 = {3, 4, 5, 4};
         System.out.println("Unique elements in array 1");
         for (int i = 0; i < arr1.length; i++) {
             for (int j = 0; j < arr2.length; j++) {
-                if(arr1[i] != arr2[j])
-                {
-                if(j == arr2.length - 1)
-                {
-                    System.out.println(arr1[i] + " is Unique");
+                if (arr1[i] != arr2[j]) {
+                    if (j == arr2.length - 1) {
+                        System.out.println(arr1[i] + " is Unique");
+                    }
+                } else {
+                    break;
                 }
-            }
-                else
-                break;
             }
         }
         System.out.println("Unique elements in array 2");
         for (int i = 0; i < arr2.length; i++) {
             for (int j = 0; j < arr1.length; j++) {
-                if(arr2[i] != arr1[j])
-                {
-                if(j == arr1.length - 1)
-                {
-                    System.out.println(arr2[i] + " is Unique");
+                if (arr2[i] != arr1[j]) {
+                    if (j == arr1.length - 1) {
+                        System.out.println(arr2[i] + " is Unique");
+                    }
+                } else {
+                    break;
                 }
-            }
-                else
-                break;
             }
         }
     }
