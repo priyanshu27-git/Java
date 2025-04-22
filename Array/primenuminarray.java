777835f7f76fa0604f9f18b664f0bc7c5815b2a3
priyanshu27-git <sahupriyanshu888a@gmail.com>
2025-04-23 00:15:19 +0530

    prime number in array

--- Array/primenuminarray.java
+++ Array/primenuminarray.java
@@ -2,21 +2,18 @@
 public class primenuminarray {
 
     public static void main(String[] args) {
-        int[] a = {3, 2, 34, 32, 23, 12};
-        int count = 0;
+        int[] a = {3, 4, 34, 32, 23, 12};
+        //boolean b = false;
         for (int i = 0; i < a.length; i++) {
-            int temp = 2;
-            for (int j = 0; j < a[j]; j++) {
-                if (a[j] % temp == 0) {
-                    count++;
-                    temp++;
+            for (int j = 2; j < a[i]; j++) {
+                if (a[i] % j == 0) {
+                    System.out.println(a[i] + " is not prime");
+                    break;
+                } else {
+                    System.out.println(a[i] + " is prime ");
                     break;
                 }
-                System.out.println( a[j] + " is not a prime number");
             }
-        }
-        if (count != 0) {
-            System.out.println(n + " is prime number");
         }
     }
 }
