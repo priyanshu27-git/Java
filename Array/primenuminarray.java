1146b16a8d3d3e26fa5d663e3b8c4a92d29dad7a
priyanshu27-git <sahupriyanshu888a@gmail.com>
2025-04-23 01:10:31 +0530

    modified prime num

--- Array/primenuminarray.java
+++ Array/primenuminarray.java
@@ -1,18 +1,51 @@
 
+// public class primenuminarray {
+
+//     public static void main(String[] args) {
+//         int[] a = {8, 5, 49, 9, 3, 2};
+//         boolean b = true;
+//         for (int i = 0; i < a.length; i++) {
+//             if (a[i] <= 1) {
+//                 b = false;
+//             }else{
+//             for (int j = 2; j < a[i] / 2; j++) {
+//                 if (a[i] % j == 0) {
+//                     b = false;
+//                     break;
+//                 }
+//             }
+//             }
+//             if (b) {
+//                 System.out.println( a[i] + " Number is prime");
+//             } else {
+//                 System.out.println(a[i] + " Number is not prime");
+//             }
+//         }
+//     }
+// }
 public class primenuminarray {
 
     public static void main(String[] args) {
-        int[] a = {3, 4, 34, 32, 23, 12};
-        //boolean b = false;
+        int[] a = {2, 2, 2, 9, 2, 2};
+
         for (int i = 0; i < a.length; i++) {
-            for (int j = 2; j < a[i]; j++) {
-                if (a[i] % j == 0) {
-                    System.out.println(a[i] + " is not prime");
-                    break;
-                } else {
-                    System.out.println(a[i] + " is prime ");
-                    break;
+            boolean isPrime = true;
+
+            if (a[i] <= 1) {
+                isPrime = false;
+            } else {
+                for (int j = 2; j <= Math.sqrt(a[i]); j++) {
+                    if (a[i] % j == 0) {
+                        isPrime = false;
+                        break;
+                    }
                 }
+            }
+
+            if (isPrime) {
+                System.out.println(a[i] + " is prime");
+            } else {
+                System.out.println(a[i] + " is not prime");
             }
         }
     }
