f33386febac46523b6b1952bdde20a163b7287e9
priyanshu27-git <sahupriyanshu888a@gmail.com>
2025-04-22 14:00:54 +0530

    vowel in string

---
+++ Array/checkvowelinstring.java
@@ -0,0 +1,14 @@
+public class checkvowelinstring {
+    public static void main(String[] args) {
+        String palindrome = "racecarnskf";
+        int count = 0;
+        char [] ch = palindrome.toCharArray();
+        for (int i = 0; i < ch.length; i++) {
+            if(ch[i] == 'a' | ch[i] == 'e' | ch[i] == 'i' | ch[i] == 'o' | ch[i] == 'u'){
+                count++;
+            }
+        }
+        System.out.println(count);
+        
+    }
+}
