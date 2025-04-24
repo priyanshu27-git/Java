ca1968214cf56326992735165b650404c8645b54
priyanshu27-git <sahupriyanshu888a@gmail.com>
2025-04-24 15:08:57 +0530

    last commit 24april

--- Patterns/ultapyramid.java
+++ Patterns/ultapyramid.java
@@ -5,13 +5,13 @@ public class ultapyramid {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
         for(int i=n;i>=0;i--){
-            for (int j = 0; j < n-i-1 ;j++) {
+            for (int j = 0; j < n-i ;j++) {
                 System.out.print(" ");
             }
             for (int j = 1; j < 2*(i+1) ;j++) {
                 System.out.print("*");
             }
-            for (int j = 0; j < n-i-1 ;j++) {
+            for (int j = 0; j < n-i ;j++) {
                 System.out.print(" ");
             }
             System.out.println();
