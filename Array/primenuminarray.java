a840645e0cb95647f9777993d5a7405f58c2868e
priyanshu27-git <sahupriyanshu888a@gmail.com>
2025-04-24 15:07:05 +0530

    8th commit 24april

--- Array/primenuminarray.java
+++ Array/primenuminarray.java
@@ -2,23 +2,24 @@
 public class primenuminarray {
 
     public static void main(String[] args) {
-        int[] a = {8, 5, 49, 9, 3, 2};
-        boolean b = true;
+        int[] a = {8, 5, 49, 1, 3, 2};
         for (int i = 0; i < a.length; i++) {
-            if (a[i] <= 1) {
+            boolean b = true;
+            int num = a[i];
+            if (num <= 1) {
                 b = false;
             } else {
-                for (int j = 2; j < a[i]; j++) {
-                    if (a[i] % j == 0) {
+                for (int j = 2; j < num; j++) {
+                    if (num % j == 0) {
                         b = false;
                         break;
                     }
                 }
             }
             if (b) {
-                System.out.println(a[i] + " Number is prime");
+                System.out.println(num+ " Number is prime");
             } else {
-                System.out.println(a[i] + " Number is not prime");
+                System.out.println(num + " Number is not prime");
             }
         }
     }
