cdc8b7e956c2846c82e678625b31201af5d3930f
priyanshu27-git <sahupriyanshu888a@gmail.com>
2025-04-24 15:08:28 +0530

    12th commit 24april

--- Patterns/rightTriangle.java
+++ Patterns/rightTriangle.java
@@ -4,8 +4,8 @@ public class rightTriangle {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter n=");
         int n = scanner.nextInt();
-        for(int i=1;i<=n;i++){
-            for(int j=0;j<i;j++){
+        for(int i=0;i<n;i++){
+            for(int j=0;j<=i;j++){
             System.out.print("*");
             }
             System.out.print("\n");
