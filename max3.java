5a57e3e30aa7374611a191a9d48128f089b328d7
priyanshu27-git <sahupriyanshu888a@gmail.com>
2025-04-24 15:04:59 +0530

    2nd commit

--- max3.java
+++ max3.java
@@ -7,13 +7,13 @@ public class max3 {
         b = scanner.nextInt();
         c = scanner.nextInt();
         if(a>b && a>c){
-            System.out.println(+a);
+            System.out.println(+a + " is greater");
         }
         else if(b>a && b>c){
-            System.out.println(+b);
+            System.out.println(+b + " is greater");
         }
         else {
-            System.out.println(+c);
+            System.out.println(+c + " is greater");
         }
     }
 }
