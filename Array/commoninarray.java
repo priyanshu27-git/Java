7a79b4662d15c0703b18f0d0e84a3d3b59cef3ed
priyanshu27-git <sahupriyanshu888a@gmail.com>
2025-04-24 15:06:40 +0530

    6th commit 24april

--- Array/commoninarray.java
+++ Array/commoninarray.java
@@ -1,7 +1,7 @@
 public class commoninarray {
     public static void main(String[] args) {
         int[] arr1 = {1, 2, 3, 4};
-        int[] arr2 = {3, 4, 2, 5};
+        int[] arr2 = {3, 1, 2, 5};
         for (int i = 0; i < arr1.length; i++) {
             for (int j = 0; j < arr2.length; j++) {
 
--- Array/compareequaltwoarray.java
+++ Array/compareequaltwoarray.java
@@ -1,18 +1,20 @@
 public class compareequaltwoarray {
     public static void main(String[] args) {
         int[] arr1 = {1, 2, 3, 4, 5};
-        int[] arr2 = {1, 2, 3, 4, 5};
+        int[] arr2 = {1, 2, 7, 9, 5};
         boolean equal = true;
 
         for (int i = 0; i < arr1.length; i++) {
-            if (arr1[i] == arr2[i]){
-                equal = true;
-            }
-            else{
+            if (arr1.length != arr2.length) {
                 equal = false;
+                break;
+            }
+            else if(arr1[i] != arr2[i]){
+                equal = false;
+                break;
             }
         }
-        if(equal==true){
+        if(equal){
         System.out.println("Elements in Array are same");
         }
     else{
