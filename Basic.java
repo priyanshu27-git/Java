f678efa1e3ac4c3873f0e9f523d59c8a0bdc3586
priyanshu27-git <sahupriyanshu888a@gmail.com>
2025-04-24 15:04:19 +0530

    1 commit 24april

--- Basic.java
+++ Basic.java
@@ -1,9 +1,9 @@
 public class Basic {
     public static void main(String[] args) {
-        byte a = 20;
-        byte  b= 30;
-        int  c = (byte)(a+b);
-        System.out.println(c);
+         byte a = (byte)128;
+        // byte  b= 30;
+        // int  c = (a+b);
+        System.out.println(a);
         // int a = 5;
         // int b = 4;
         // System.out.println(a&b);
--- evenOdd.java
+++ evenOdd.java
@@ -1,16 +1,17 @@
+
 import java.util.Scanner;
+
 public class evenOdd {
-    public static void main(String[] args)throws Exception {
+
+    public static void main(String[] args) {
         int n;
         Scanner scanner = new Scanner(System.in);
         n = scanner.nextInt();
-        if(n == 0){
+        if (n == 0) {
             System.out.println("Zero");
-        }
-        else if(n % 2 == 0){
+        } else if (n % 2 == 0) {
             System.out.println("Even");
-        }
-        else if(n % 2 != 0){
+        } else if (n % 2 != 0) {
             System.out.println("Odd");
         }
     }
