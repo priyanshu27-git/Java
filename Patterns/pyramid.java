d7dd833d504519b582506cb25fd3130b9f473962
priyanshu27-git <sahupriyanshu888a@gmail.com>
2025-04-24 15:08:19 +0530

    11th commit 24april

--- Patterns/pyramid.java
+++ Patterns/pyramid.java
@@ -6,13 +6,13 @@ class pyramid {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
         for(int i=0;i<n;i++){
-            for (int j = 0; j < n-i-1 ;j++) {
+            for (int j = 0; j < n-i ;j++) {
                 System.out.print(" ");
             }
             for (int j = 1; j < 2*(i+1) ;j++) {
                 System.out.print("*");
             }
-            for (int j = 0; j < n-i-1 ;j++) {
+            for (int j = 0; j < n-i ;j++) {
                 System.out.print(" ");
             }
             System.out.println();
