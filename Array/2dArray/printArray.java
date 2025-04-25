f4ced1ae04a2dc7e60efdba5128009f6485902e7
priyanshu27-git <sahupriyanshu888a@gmail.com>
2025-04-25 10:25:24 +0530

    print2d array

--- Array/2dArray/printArray.java
+++ Array/2dArray/printArray.java
@@ -1,11 +1,17 @@
+
 public class printArray {
+
     public static void main(String[] args) {
-        int [][] a = {{1,2,3,4},{3,4,5,6}};
-            for (int i = 0; i < a.length; i++) {
-                for (int j = 0; j < a[i].length; j++) {
-                    System.out.print(a[i][j] + " ");
-                }
-                System.out.println();
+        int[][] a = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+        for(int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[i].length; j++) {
+                System.out.print(a[i][j] + " ");
             }
+            System.out.println();
+        }
     }
 }
