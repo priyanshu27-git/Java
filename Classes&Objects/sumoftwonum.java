6c00ca8cdc5f7e2722c9495439e16d3293f01955
priyanshu27-git <sahupriyanshu888a@gmail.com>
2025-04-25 12:40:49 +0530

    sum of two num

---
+++ Classes&Objects/sumoftwonum.java
@@ -0,0 +1,12 @@
+public class sumoftwonum {
+    void sum(){
+        int a = 2;
+        int b = 3;
+        int c = a+b;
+        System.out.println("Sum of two num= "+c);
+    }
+    public static void main(String[] args) {
+        sumoftwonum obj = new sumoftwonum();
+        obj.sum();
+    }
+}
