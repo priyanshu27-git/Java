40bad7eb723ef7c1971d5862f0fe91f9d372ec56
priyanshu27-git <sahupriyanshu888a@gmail.com>
2025-04-25 10:21:11 +0530

    Stringbuilder first program

---
+++ String/Stringbuilderbasic.java
@@ -0,0 +1,9 @@
+public class Stringbuilderbasic {
+    public static void main(String[] args) {
+        StringBuilder str = new StringBuilder(10);
+        str.append("Priyanshu sahu");
+        System.out.println(str);
+        System.out.println(str.length());
+        System.out.println(str.capacity());
+    }
+}
