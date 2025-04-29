71f0031860dcc72a045e4a706d994ee3c5f913e8
priyanshu27-git <sahupriyanshu888a@gmail.com>
2025-04-29 22:55:56 +0530

    natural numbers

---
+++ Assignment/Naturalnumber.java
@@ -0,0 +1,10 @@
+public class Naturalnumber {
+    public static void printnum(int n){
+         for (int i = 1; i <= n; i++) {
+             System.out.println(i);
+         }
+    }
+    public static void main(String[] args) {
+        printnum(10);
+    }
+}
