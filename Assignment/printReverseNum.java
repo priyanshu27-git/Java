418facc3da54877ac2c0f05bd232feefd49e1995
priyanshu27-git <sahupriyanshu888a@gmail.com>
2025-04-29 22:57:27 +0530

    print reverse nums

---
+++ Assignment/printReverseNum.java
@@ -0,0 +1,10 @@
+public class printReverseNum {
+    public static void printnum(int n){
+        for (int i = n; i >= 0; i--) {
+            System.out.println(i);
+        }
+   }
+   public static void main(String[] args) {
+       printnum(12);
+   }
+}
