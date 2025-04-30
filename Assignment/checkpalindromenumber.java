7bb1edda6f3b3f08357cf39e481622329b549bad
priyanshu27-git <sahupriyanshu888a@gmail.com>
2025-04-30 14:30:37 +0530

    check number is plaindrome or not

---
+++ Assignment/checkpalindromenumber.java
@@ -0,0 +1,23 @@
+
+public class checkpalindromenumber {
+
+    public static void palindrome(int n) {
+        int temp = n;
+        int rev = 0;
+        while (temp > 0) {
+            int digits = temp % 10;
+            rev = rev * 10 + digits;
+            temp /= 10;
+        }
+        System.out.println(rev);
+        if (rev != n) {
+            System.out.println(n + " is not palindrome ");
+        } else {
+            System.out.println(n + " is Palindrome ");
+        }
+    }
+
+    public static void main(String[] args) {
+        palindrome(4994);
+    }
+}
