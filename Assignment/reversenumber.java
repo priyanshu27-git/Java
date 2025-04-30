8a1f4d09c7b94f35b05c674edce209e6363302fd
priyanshu27-git <sahupriyanshu888a@gmail.com>
2025-04-30 14:32:54 +0530

    reverse number

---
+++ Assignment/reversenumber.java
@@ -0,0 +1,14 @@
+public class reversenumber {
+    public static void reverse(int n) {
+        int rev = 0;
+    while(n > 0){
+        int digits = n % 10;
+        rev = rev * 10 + digits ;
+        n /= 10; 
+    }
+    System.out.println("Reverse number = " +rev);
+    }
+    public static void main(String[] args) {
+        reverse(123);
+    }
+}
