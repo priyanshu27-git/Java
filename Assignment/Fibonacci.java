b97d4aa4cc8152df37d12cf3bbd16f24c6288af7
priyanshu27-git <sahupriyanshu888a@gmail.com>
2025-05-01 13:48:37 +0530

    Fibonacci number

---
+++ Assignment/Fibonacci.java
@@ -0,0 +1,14 @@
+public class Fibonacci {
+    public static void fibonacci(int n) {
+        int a = 0, b = 1, c = 0;
+        for (int i = 0; c <= n; i++) {
+            System.out.print(c + " ");
+            a = b;
+            b = c;
+            c = a + b;
+        }
+    }
+    public static void main(String[] args) {
+        fibonacci(25);
+    }
+}
