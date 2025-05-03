8ac2fa3f1e96bc5cdd5d91f691643910d91bf42d
priyanshu27-git <sahupriyanshu888a@gmail.com>
2025-05-03 16:17:59 +0530

    Armstrong Number

--- Assignment/CheckArmstrongNumber.java
+++ Assignment/CheckArmstrongNumber.java
@@ -10,22 +10,32 @@ public class CheckArmstrongNumber {
         }
         //System.out.println(count);
         //Finding Armstrong number
-        int divisors = 1, temp2 = n;
+        //int divisors = 1;
+        int temp2 = n;
         int ans = 0;
-        while (temp2 > 10) {
-            temp2 = temp2 / 10;
-            divisors *= 10;// making divisors same digit as of n
-        }
-        int temp = n;
-        while (temp > 0) {
+        // while (temp2 > 10) {
+        //     temp2 = temp2 / 10;
+        //     divisors *= 10;// making divisors same digit as of n
+        // }
+        // int temp = n;
+        // while (temp > 0) {
+        //     int pow = 1;
+        //     int digits2 = temp / divisors;
+        //     for (int i = 0; i < count; i++) {
+        //         pow = pow * digits2;
+        //     }
+        //     temp = temp % divisors;
+        //     divisors /= 10;
+        //     ans = ans + pow;
+        // }
+        while(temp2 > 0){
             int pow = 1;
-            int digits2 = temp / divisors;
+            int digits2 = temp2 % 10;
             for (int i = 0; i < count; i++) {
-                pow = pow * digits2;
+                pow *= digits2;
             }
-            temp = temp % divisors;
-            divisors /= 10;
-            ans = ans + pow;
+            ans += pow;
+            temp2 /= 10;
         }
         if (ans == n) {
             System.out.println(ans + " is Armstrong");
