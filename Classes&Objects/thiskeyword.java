4c8ff4cbbf21d6f6fa5abf19b5b6b4d00e7be9e8
priyanshu27-git <sahupriyanshu888a@gmail.com>
2025-05-09 09:53:53 +0530

    use of this keyword

---
+++ Classes&Objects/thiskeyword.java
@@ -0,0 +1,14 @@
+public class thiskeyword {
+    int a;
+
+    public thiskeyword(int a) {
+        System.out.println(a);
+    }
+    public static void main(String[] args) {
+        thiskeyword obj = new thiskeyword(2);
+        System.out.println(obj.a);
+        System.out.println(obj);
+        
+    }
+    
+}
