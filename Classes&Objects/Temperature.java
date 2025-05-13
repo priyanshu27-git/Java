7bdb6840bad3260c7992ee92a3f563aa4db24ee3
priyanshu27-git <sahupriyanshu888a@gmail.com>
2025-05-13 23:00:49 +0530

    Encapsulation in temperature

---
+++ Classes&Objects/Temperature.java
@@ -0,0 +1,16 @@
+public class Temperature {
+    float celsius;
+    void settemperature(int c){
+        celsius = c;
+        celsius = (celsius * 9/5) + 32;
+    }
+    float gettemperature(){
+        return celsius;
+    }
+    public static void main(String[] args) {
+        Temperature obj = new Temperature();
+        obj.settemperature(36);
+        System.out.println(obj.gettemperature());
+    }
+    
+}
