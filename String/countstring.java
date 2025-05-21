a13ff95b6831381833ab52025a84131715be9304
priyanshu27-git <sahupriyanshu888a@gmail.com>
2025-05-21 13:24:32 +0530

    Count number of alphabets

---
+++ String/countstring.java
@@ -0,0 +1,19 @@
+public class countstring {
+    public static void main(String[] args) {
+        String str = "Aaaabbbaacc";
+        int count = 1;
+        StringBuilder newStr = new StringBuilder();
+        for (int i = 1; i < str.length(); i++) {
+            if(str.charAt(i) == str.charAt(i-1)){
+                count++;
+            }
+            else{
+                newStr.append(str.charAt(i-1)).append(count);
+                count = 1;
+            }
+        }
+        newStr.append(str.charAt(str.length()-1)).append(count);
+        System.out.println(newStr);
+    }
+}
+
