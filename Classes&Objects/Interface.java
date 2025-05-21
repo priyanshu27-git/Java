75af47f4d2b00bd8acdd35bb430b20efd5e9f082
priyanshu27-git <sahupriyanshu888a@gmail.com>
2025-05-21 23:55:09 +0530

    Interface q1

---
+++ Classes&Objects/Interface.java
@@ -0,0 +1,19 @@
+    interface Client{
+        void print();
+        void show();
+    }
+     class Dev implements Client{
+        public void print(){
+            System.out.println("Billl");
+        }
+        public void show(){
+            System.out.println("biryanii");
+        }
+    }
+    public class Interface{
+    public static void main(String[] args) {
+        Dev obj = new Dev();
+        obj.print();
+        obj.show();
+    }
+}
