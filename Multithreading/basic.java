5761733af196deb3c9d52b23d5163b6b0ea1263a
priyanshu27-git <sahupriyanshu888a@gmail.com>
2025-06-03 18:26:10 +0530

    Multithreading first program

---
+++ Multithreading/basic.java
@@ -0,0 +1,13 @@
+class myThread extends Thread{
+   public void run(){
+        System.out.println("hello ");
+    }
+}
+
+public class basic {
+    public static void main(String[] args) {
+        // myThread t1 = new myThread();
+        // t1.start();
+        // System.out.println("heyyy");
+    }
+}
